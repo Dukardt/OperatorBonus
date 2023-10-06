68b01c9414a55d450c8e152e05b48a6a06bd4757
Valery Dukardt <valeradukardt@mail.ru>
2023-10-06 17:21:41 +0700

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
         if (replenished >= 1000) {
             bonus = (int) Math.floor(replenished / 100) * 1;
             balance += replenished + bonus;
-        }else{
+        } else {
             balance += replenished;
 
         }
 
-        System.out.println("Новый баланс"+balance);
+        System.out.println("Новый баланс" + balance);
 
-        System.out.println("Начисленно бонусов"+bonus);
+        System.out.println("Начисленно бонусов" + bonus);
 
         // Новый баланс1712
         //Начисленно бонусов12 (При пополнении на 1200 т.е >1000)
